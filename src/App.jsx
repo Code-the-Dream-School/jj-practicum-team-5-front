8cf6fabb452b47dcd879d3c4de4cb92578a07a1c
AnnaBazileeva <anna.bazileeva@gmail.com>
2025-08-15 13:26:07 -0400

    add dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import MainLayout from "./layouts/MainLayout.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import SignUpForm from "./pages/SignUpForm.jsx";
+import Dashboard from "./pages/Dashboard.jsx"
 import './index.css';
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
                 <Route element={<MainLayout />}>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/login" element={<LoginPage />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
                     <Route path="/signup" element={<SignUpForm />} />
                 </Route>
             </Routes>
